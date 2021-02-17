82131a61ce7a079eaa5533010c5b9a5a9e3ecfc7
Tushar Agrawal <tusharagrawal16@rediffmail.com>
2021-02-18 01:06:52 +0530

    Moved major backend

---
+++ app.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+const express = require("express"),
+  request = require("request"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  myRoutes = require("./routes"),
+  PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use("/api", myRoutes);
+
+app.get("/", (req, res) => {
+  res.sendFile("./public/index.html", { root: __dirname });
+});
+app.get("/iamadmin", (req, res) => {
+  res.sendFile("./public/moderator.html", { root: __dirname });
+});
+app.get("/getquote", (req, res) => {
+  var url = "https://api.adviceslip.com/advice";
+  request(url, (err, result, body) => {
+    if (err) throw err;
+    else {
+      var red = JSON.parse(body);
+      res.send(red.slip.advice);
+    }
+  });
+});
+
+app.post("*", (req, res) => {
+  res.send("Not Found the Page you are looking for");
+});
+
+app.listen(PORT, () => {
+  console.log(`listening to port ${PORT}`);
+});
---
+++ routes/index.js
@@ -0,0 +1,83 @@
+const express = require("express");
+
+
+router = express.Router();
+
+router.get("/getloaders", (req, res) => {
+  connection.query("select * from loaders", function (err, result) {
+    if (err) {
+      res.send("ERROR");
+    }
+    res.send(result);
+  });
+});
+router.post("/setpopup", (req, res) => {
+  connection.query("select * from others", function (err, result) {
+    if (req.body.password === result[1].content) {
+      connection.query("UPDATE others SET content = ? WHERE mtype = 'MESSAGE'", req.body.message, function (err, result) {
+        res.send(result);
+      });
+    } else {
+      res.send("Wrong Password");
+    }
+  });
+});
+
+router.get("/serveranypopup", (req, res) => {
+  connection.query("select content from others where mtype='MESSAGE'", function (err, result) {
+    if (err) res.send("NO");
+    else res.send(result[0].content);
+  });
+});
+
+router.post("/addthiscode", (req, res) => {
+  var post = req.body;
+  console.log(post);
+  connection.query("INSERT INTO loaders SET ?", post, (err, result) => {
+    if (err) res.send(err);
+    else res.send(result);
+  });
+});
+router.post("/updatecode", (req, res) => {
+  connection.query("Select * from others where mtype = 'PASSWORD'", (err, result) => {
+    if (req.body.password != result[0].content) {
+      res.send("NO");
+    } else {
+      connection.query("UPDATE loaders SET html = ?, css = ?, lname =?, contributor = ? where loaderid = ?", [req.body.html, req.body.css, req.body.lname, req.body.contributor, req.body.loaderid], function (err, result) {
+        if (err) {
+          console.log(err);
+          res.send("Error");
+        } else res.send("Done");
+      });
+    }
+  });
+});
+
+router.put("/like/:loader", (req, res) => {
+  var val = req.body.like == "true" ? 1 : -1;
+  connection.query("UPDATE loaders SET likes = likes + ? WHERE loaderid = ?", [val, req.params.loader], (err, result) => {
+    if (err) res.send(err);
+    else res.json({ val: val });
+  });
+});
+
+router.post("/imgoingtodeletethispost", (req, res) => {
+  connection.query("Select * from others where mtype = 'PASSWORD'", (err, result) => {
+    if (req.body.password != result[0].content) {
+      res.send("wrong password");
+    } else {
+      connection.query("DELETE from loaders WHERE loaderid = ?", req.body.loaderid, (err, result) => {
+        if (err) {
+          console.log(err);
+          res.send(err);
+        } else res.send("Done");
+      });
+    }
+  });
+});
+router.get("/setthis", (req, res) => {
+  connection.query("INSERT INTO others values('MESSAGE','NO')", function (err, result, field) {
+    res.send(result);
+  });
+});
+module.exports = router;
